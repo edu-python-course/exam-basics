8843cad01158ed5911f7bb76e82a7206dced67ee
Serhii Horodilov <sgorodil@gmail.com>
2023-01-31 03:10:35 +0200

    Added brick wall generator helper function
    Helper goal is to provide some data for developers.
    This function does not provide the correct answers.
    Generated structures are random and not measured.

--- src/exam/brick_wall.py
+++ src/exam/brick_wall.py
@@ -3,7 +3,7 @@ Brick wall challenge
 ====================
 
 """
-
+import random
 from typing import List
 
 
@@ -31,3 +31,37 @@ def get_weak_line(structure: List[List[int]]) -> int:
     :rtype: int
 
     """
+
+
+def generate_wall(rows: int = 5, min_row_length: int = 15) -> List[List[int]]:
+    """Generates a wall structure for a challenge
+
+    :param rows: number of rows in a wall structure to generate.
+        Defaults to 5.
+    :param min_row_length: minimum desired length of a single row in a wall
+        structure. Defaults to 15.
+    :return: a generated wall structure
+
+    """
+
+    structure: List[List[int]] = []
+    max_row_length: int = 0
+    for row in range(rows):
+        structure_row: List[int] = []
+        structure_row_length: int = 0
+
+        while structure_row_length < min_row_length:
+            brick_length = random.randint(1, 5)
+            structure_row.append(brick_length)
+            structure_row_length += brick_length
+
+        max_row_length = max(structure_row_length, max_row_length)
+        structure.append(structure_row)
+
+    # adjust rows length
+    for structure_row in structure:
+        structure_row_length = sum(structure_row)
+        if structure_row_length < max_row_length:
+            structure_row.append(max_row_length - structure_row_length)
+
+    return structure
