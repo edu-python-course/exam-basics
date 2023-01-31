426dc38638a3fe6002ea2c61b87d23b33885ce4f
Serhii Horodilov <sgorodil@gmail.com>
2023-01-31 02:14:38 +0200

    Added base function declarations (brick wall challenge)

---
+++ src/exam/brick_wall.py
@@ -0,0 +1,33 @@
+"""
+Brick wall challenge
+====================
+
+"""
+
+from typing import List
+
+
+# noinspection PyUnusedLocal
+def get_brick_count(structure: List[list[int]]) -> int:
+    """Return number of bricks in the wall structure
+
+    :param structure: represents wall structure as sequences of integers
+    :type structure: list[list[int]]
+
+    :return: total number of bricks in the entire wall structure
+    :rtype: int
+
+    """
+
+
+# noinspection PyUnusedLocal
+def get_weak_line(structure: List[List[int]]) -> int:
+    """Return a pointer to the weakest line in the structure
+
+    :param structure: represents wall structure as sequences of integers
+    :type structure: list[list[int]]
+
+    :return: the distance from the left edge to the weakest line location
+    :rtype: int
+
+    """
