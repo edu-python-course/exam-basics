259f1e07b893eb255acf01219f9d496dd6861749
Serhii Horodilov <sgorodil@gmail.com>
2023-02-01 12:25:15 +0200

    Resolved merge conflicts while rebasing
    - src/exam/brick_wall.py
    - tests/conftest.py
    - poetry.lock

--- src/exam/brick_wall.py
+++ src/exam/brick_wall.py
@@ -3,12 +3,13 @@ Brick wall challenge
 ====================
 
 """
+
 import random
 from typing import List
 
 
 # noinspection PyUnusedLocal
-def get_brick_count(structure: List[list[int]]) -> int:
+def get_bricks_count(structure: List[list[int]]) -> int:
     """Return number of bricks in the wall structure
 
     :param structure: represents wall structure as sequences of integers
@@ -21,13 +22,26 @@ def get_brick_count(structure: List[list[int]]) -> int:
 
 
 # noinspection PyUnusedLocal
-def get_weak_line(structure: List[List[int]]) -> int:
+def get_least_bricks_position(structure: List[List[int]]) -> int:
     """Return a pointer to the weakest line in the structure
 
     :param structure: represents wall structure as sequences of integers
     :type structure: list[list[int]]
 
     :return: the distance from the left edge to the weakest line location
+    :rtype: int
+
+    """
+
+
+# noinspection PyUnusedLocal
+def get_least_bricks_count(structure: List[List[int]]) -> int:
+    """Return the least number of bricks in a line
+
+    :param structure: represents wall structure as sequences of integers
+    :type structure: list[list[int]]
+
+    :return: the least number of bricks crossed by a vertical line
     :rtype: int
 
     """
--- tests/brick_wall_test.py
+++ tests/brick_wall_test.py
@@ -5,13 +5,16 @@ import pytest
 from exam import brick_wall
 
 
-@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="exam development env")
-def test_brick_counter(regular_brick_wall):
-    fixture, _, bricks_count = regular_brick_wall
-    assert brick_wall.get_brick_count(fixture) == bricks_count
+@pytest.mark.xfal(os.environ.get("ENV_DEV"), reasone="development environment")
+def test_small_wall_counter():
+    assert brick_wall.get_bricks_count([[1], [1], [1]]) == 3
 
 
-@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="exam development env")
-def test_matrix_builder(regular_brick_wall):
-    fixture, line_position, _ = regular_brick_wall
-    assert brick_wall.get_weak_line(fixture) == line_position
+@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="development environment")
+def test_brick_counter(bricks_wall, number_of_bricks):
+    assert bricks_wall.get_bricks_count(bricks_wall) == number_of_bricks
+
+
+@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="development environment")
+def test_get_least_bricks_position(bricks_wall, position):
+    assert bricks_wall.get_least_bricks_position(bricks_wall) == position
