1f522f6511c0ce1e594cdd0865e735010494b48b
Serhii Horodilov <sgorodil@gmail.com>
2023-02-10 09:18:07 +0200

    Added dataset filter
    Added dummy function, fixtures and tests.
    Filter function has description.

---
+++ tests/dataset_test.py
@@ -0,0 +1,44 @@
+import os
+import timeit
+
+import pytest
+
+from exam.dataset import filter_by_values
+
+
+@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="development environment")
+@pytest.mark.dependency()
+def test_dataset_f1(dataset, filter_1):
+    filtered, *keys = filter_1
+    assert filter_by_values(dataset, keys) == filtered
+
+
+@pytest.mark.dependency(depends=["test_dataset_f1"])
+def test_dataset_f1_benchmark():
+    time_limit = 0.5
+    timeit_num = 3
+    setup = "from exam.dataset import filter_by_values; " \
+            "from tests.conftest import DATASET, KEYS_1"
+    stmt = "filter_by_values(DATASET)"
+    benchmark = timeit.timeit(stmt, setup, number=timeit_num)
+    assert benchmark <= time_limit, \
+        f"execution time exceeded time limit: {time_limit}"
+
+
+@pytest.mark.xfail(os.environ.get("ENV_DEV"), reason="development environment")
+@pytest.mark.dependency()
+def test_dataset_f2(dataset, filter_2):
+    filtered, *keys = filter_2
+    assert filter_by_values(dataset, keys) == filtered
+
+
+@pytest.mark.dependency(depends=["test_dataset_f2"])
+def test_dataset_f2_benchmark():
+    time_limit = 0.5
+    timeit_num = 3
+    setup = "from exam.dataset import filter_by_values; " \
+            "from tests.conftest import DATASET, KEYS_2"
+    stmt = "filter_by_values(DATASET, KEYS_2)"
+    benchmark = timeit.timeit(stmt, setup, number=timeit_num)
+    assert benchmark <= time_limit, \
+        f"execution time exceeded time limit: {time_limit}"
