c632a84e53135566325ec7bff61c5bc58cd63273
Serhii Horodilov <sgorodil@gmail.com>
2023-02-10 11:30:41 +0200

    Updated env settings

--- tests/brick_wall_test.py
+++ tests/brick_wall_test.py
@@ -5,18 +5,21 @@ import pytest
 from exam import brick_wall
 
 
-@pytest.mark.xfail(os.environ.get("DEV"), reason="development environment")
+@pytest.mark.xfail(os.environ.get("ENV") == "dev",
+                   reason="development environment")
 def test_small_brick_counter(small_fixture):
     structure, number_of_bricks = small_fixture
     assert brick_wall.get_bricks_count(structure) == number_of_bricks
 
 
-@pytest.mark.xfail(os.environ.get("DEV"), reason="development environment")
+@pytest.mark.xfail(os.environ.get("ENV") == "dev",
+                   reason="development environment")
 def test_brick_counter(regular_fixture):
     structure, number_of_bricks = regular_fixture
     assert brick_wall.get_bricks_count(regular_fixture) == number_of_bricks
 
 
-@pytest.mark.xfail(os.environ.get("DEV"), reason="development environment")
+@pytest.mark.xfail(os.environ.get("ENV") == "dev",
+                   reason="development environment")
 def test_get_least_bricks_position(regular_fixture, position):
     assert brick_wall.get_least_bricks_position(regular_fixture) == position
