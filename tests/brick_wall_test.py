86b6cd40ca606d2976368de200de6de24afe2786
Serhii Horodilov <sgorodil@gmail.com>
2023-02-10 14:34:13 +0200

    Updated test cases for brick wall challenge
    Added least number of bricks test case.

--- tests/brick_wall_test.py
+++ tests/brick_wall_test.py
@@ -24,5 +24,15 @@ def test_brick_counter(regular_fixture):
 @pytest.mark.xfail(os.environ.get("ENV") == "dev",
                    reason="development environment")
 @pytest.mark.level_2
+@pytest.mark.dependency()
 def test_get_least_bricks_position(regular_fixture, position):
     assert brick_wall.get_least_bricks_position(regular_fixture) == position
+
+
+@pytest.mark.xfail(os.environ.get("ENV") == "dev",
+                   reason="development environment")
+@pytest.mark.level_2
+@pytest.mark.dependency(depends=["test_get_least_bricks_position"])
+def test_least_bricks(regular_fixture, small_fixture):
+    assert brick_wall.get_least_bricks_count(regular_fixture) == 1
+    assert brick_wall.get_least_bricks_count(small_fixture) == 3
