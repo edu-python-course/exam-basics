e59cab1f7fe4ad6e9ec2657ee504d96fb1c0e9c0
Serhii Horodilov <sgorodil@gmail.com>
2023-02-10 13:36:38 +0200

    Updated ds filter documentation

--- src/exam/dataset.py
+++ src/exam/dataset.py
@@ -9,37 +9,32 @@ from typing import Any, Dict, List, Optional
 # noinspection PyUnusedLocal
 def filter_by_values(origin: List[Dict[str, Any]],
                      keys: Optional[List[str]] = ...) -> List[Dict[str, Any]]:
-    """Return a filtered dataset using unique values in given keys
+    """Return a filtered datasets by unique values in a given keys sets
 
-    :param origin: origin dataset
+    :param origin: an original dataset with entries to filter
     :type origin: list
-
-    :param keys: a list of keys
+    :param keys: a collection of keys to use for filtering
     :type keys: list, optional
 
-    :return: filtered dataset
-    :rtype: list[dict]
+    :return: a filtered dataset
+    :rtype: list
 
-    The original dataset is a list that contains dictionaries.
-    All the dictionaries have the same keys of a string type.
+    The origin dataset is a list of dictionaries. All the dictionaries have
+    the same set of keys of a string type. At least one entry with at least
+    one key is granted.
 
-    The function will filter the original dataset using values in a given
-    set of keys. If some set of values has been seen before, the entire
-    dictionary should be filtered (excluded from the result list).
+    The keys parameter is used to set the dictionary keys to filter unique
+    values. Keys list is considered to be validated before passing to this
+    function, all values (if any) are valid. In case this parameter is
+    omitted - all available keys should be used.
 
-    Example:
+    Usage:
 
-    >>> filter_by_values([{"x": 1, "y": "A"}, {"x": 1, "y":"AB"}], ["x"])
-    [{"x": 1, "y": "A"}]
-    >>> filter_by_values([{"x": 1, "y": "A"}, {"x": 1, "y":"AB"}], ["x", "y"])
-    [{"x": 1, "y": "A"}, {"x": 2, "y": "AB"}]
-
-    In case ``key`` parameter has been omitted, all the available keys are
-    to be used to filter values.
-
-    Example:
-
-    >>> filter_by_values([{"x": 1, "y": "A"}, {"x": 1, "y":"AB"}])
-    [{"x": 1, "y": "A"}, {"x": 2, "y": "AB"}]
+    >>> ds = [{"x": 1, "y": 2, "z": 3}, {"x": 0, "y": 2, "z": 3}]
+    >>> assert filter_by_values(ds, ["x"]) == ds       # the same as origin
+    >>> assert filter_by_values(ds, ["x", "z"]) == ds  # the same as origin
+    >>> assert filter_by_values(ds, ["y"]) == [{"x": 1, "y": 2, "z": 3}]
+    >>> assert filter_by_values(ds, ["y", "z"]) == [{"x": 1, "y": 2, "z": 3}]
 
     """
+
